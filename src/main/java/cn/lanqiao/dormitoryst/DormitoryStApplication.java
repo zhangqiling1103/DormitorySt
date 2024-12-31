7bab200a3996f5ed818e392876ee3b6cbf6a9749
zhangqiling <3225135653@qq.com>
2024-12-31 14:30:40 +0800

    first commit

---
+++ src/main/java/cn/lanqiao/dormitoryst/DormitoryStApplication.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.dormitoryst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryStApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitoryStApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lanqiao/dormitoryst/DormitoryStApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.dormitoryst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryStApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
